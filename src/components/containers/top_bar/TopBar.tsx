c4bd5608f8bf6a3b6f38a7c6df99aff8d4ad30e7
Wojciech Teclaw <teclaw.wojciech@gmail.com>
2023-01-25 12:49:44 +0100

    [develop] Add other image + href to the page

--- src/components/containers/top_bar/TopBar.tsx
+++ src/components/containers/top_bar/TopBar.tsx
@@ -4,9 +4,11 @@ import "./TopBar.css";
 const TopBar: FC = () => {
     return (
         <div id="top-bar-container">
-            <div id="top-bar-logo-container" style={{ height: "100%", width: "auto" }}>
-                <img src="./img/sampleLogo.png" />
-            </div>
+            <a href="https://github.com/LBD-Hackers" target="_blank" style={{ height: "100%", width: "auto" }}>
+                <div id="top-bar-logo-container" style={{ height: "100%", width: "auto" }}>
+                    <img src="./img/sampleLogo.png" />
+                </div>
+            </a>
             <div className="top-bar-menu">
                 <div className="top-bar-menu-wrappers">
                     <div id="top-bar-contact-container">Paper</div>
