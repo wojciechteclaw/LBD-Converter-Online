1243c59921e1dd75c811301723f6315c7c477627
Wojciech Teclaw <teclaw.wojciech@gmail.com>
2023-02-01 16:47:25 +0100

    [feature/displayGraph] Add simple 2d graph with example data

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -1,12 +1,32 @@
-import { FC } from "react";
-import ForceGraph2D from "react-force-graph-2d";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
+import ForceGraph2D, { ForceGraphProps } from "react-force-graph-2d";
+import { exampleData } from "./exampleData";
 
 const Graph: FC = () => {
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    const forceGraphRef = useRef();
+
+    useEffect(() => {
+        setWidth((document.querySelector("#graph-container-graph") as HTMLElement).clientWidth);
+        setHeight((document.querySelector("#graph-container-graph") as HTMLElement).clientHeight);
+    }, []);
+
+    
+
     return (
         <>
-            <ForceGraph2D />
+            <ForceGraph2D
+                ref={forceGraphRef}
+                width={width}
+                height={height}
+                backgroundColor="#ced9d9"
+                graphData={exampleData}
+                linkCurvature="curvature"
+            />
         </>
     );
 };
 
-export {Graph};
+export { Graph };
