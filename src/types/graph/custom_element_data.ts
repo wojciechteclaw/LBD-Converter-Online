adcc85d1aff7ee788cce05980b3b220e78fd70de
Wojciech Teclaw <teclaw.wojciech@gmail.com>
2023-02-04 17:04:49 +0100

    [feature/cytoscapeMigration] Add types to remodel graph in cytoscape context

---
+++ src/types/graph/custom_element_data.ts
@@ -0,0 +1,8 @@
+type CustomElementData = {
+    namespace: string;
+    id: string;
+    body: string;
+    color?: string;
+};
+
+export { CustomElementData };
---
+++ src/types/graph/edge_element.ts
@@ -0,0 +1,5 @@
+import { EdgeElementData } from "./edge_element_data";
+
+type EdgeElement = { data: EdgeElementData };
+
+export { EdgeElement };
